d87166ebf8606d547feb4c390a12edbc1df1be77
mrtom222 <38867467+mrtom222@users.noreply.github.com>
2022-03-29 10:42:29 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+
+  public static void draw(int w){
+    for(int i = 1;i<=w;i++){
+      for(int j=0;j<(w-i);j++){
+        System.out.print(" ");
+      }
+      for(int j=0;j<i;j++){
+        System.out.print("*");
+      }
+      System.out.println("");
+    }
+  }
+  
+  public static void main(String[] args) {
+    int w = 4,p=3;
+    for(int i = 0;i<p;i++)
+      draw(w);
+  }
+}
